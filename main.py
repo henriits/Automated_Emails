a2f3d8baba8e88551a7dcff790bea3a7eddec55f
henriits <121551949+henriits@users.noreply.github.com>
2023-03-09 21:36:38 +0200

    getting data from api, and testing

--- main.py
+++ main.py
@@ -1,3 +1,34 @@
+from pprint import pprint
+
+# API key abdc73e693b94dae98b16b7845f97039
+
+import requests
+
+topic = "python programming"
+
+api_key = "abdc73e693b94dae98b16b7845f97039"
+url = ('https://newsapi.org/v2/everything?'
+       f'q={topic}&'
+       'sortBy=publishedAt&'
+       'apiKey=abdc73e693b94dae98b16b7845f97039&'
+       'language=en')
+
+request = requests.get(url)
+
+# Get a Dictionary with data
+content = request.json()
+
+articles = content["articles"]
+
+# Access the article titles and description
+body = ""
+for article in articles:
+    body = body + article["title"] + "\n" + article["url"] + (2 * "\n")
+
+# body = body.encode("utf-8")
+print(body)
+
+
 class NewsFeed:
 
     def __init__(self, data):
