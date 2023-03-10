b5c53b9e1f2dd0d929a948d5b463b0d5c821ddfd
henriits <121551949+henriits@users.noreply.github.com>
2023-03-10 20:51:57 +0200

    adding date time to email

--- email_file.py
+++ email_file.py
@@ -1,16 +1,30 @@
 import pandas
 import os
 import yagmail
+from main import NewsFeed
+import datetime
+
 
 SENDER = os.environ.get("MYMAIL")
 RECEIVER = os.environ.get("MYMAIL")
 PASSWORD = os.environ.get("Gmail_PASSWORD")
 
-email = yagmail.SMTP(user=SENDER, password=PASSWORD)
-email.send(to=RECEIVER,
-           subject="Hi There",
-           contents="Hi, this is a body of the email \nHenri.",
-           attachments="design.txt")
+df = pandas.read_excel(r'people.xlsx', sheet_name="Sheet 1")
 
-# if __name__ == "__main__":
-#     pass
+today = datetime.datetime.now().strftime("%Y-%m-%d")
+yesterday = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%Y-%m-%d")
+
+for index, row in df.iterrows():
+    news_feed = NewsFeed(interest=row['interest'],
+                         from_date=yesterday,
+                         to_date=today)
+    email = yagmail.SMTP(user=SENDER, password=PASSWORD)
+    email.send(to=row["email"],
+               subject=f"Your {row['interest']} news for today!",
+               contents=f"Hi, {row['name']} \n See what's on about {row['interest']} today. "
+                        f"\n"
+                        f"\n"
+                        f"\n {news_feed.get()} "
+                        f"\nHenri.")
+
+
